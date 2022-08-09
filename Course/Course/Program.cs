679449a410c6a0c5b3f3800e4d7a5cdf5e3bda1b
EdanAlmeida <edluisalmeida@gmail.com>
2022-08-09 17:35:06 -0300

    first commit

---
+++ Course/Course/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte x = 100;
+            Console.WriteLine(x);
+            long n4 = 2147483648L; //long int
+            Console.WriteLine(n4);
+        }
+    }
+}
