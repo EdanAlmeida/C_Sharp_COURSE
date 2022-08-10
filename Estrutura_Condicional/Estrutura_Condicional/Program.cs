84dc68cd4213a235623812c3569731125fd9cbd0
EdanAlmeida <edluisalmeida@gmail.com>
2022-08-10 16:59:14 -0300

    081022_3

---
+++ Estrutura_Condicional/Estrutura_Condicional/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Estrutura_Condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a hora atual? ");
+            int hora = int.Parse(Console.ReadLine());
+
+            if (hora > 0 && hora < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+            else if (hora >= 12 && hora < 18)
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+            else if(hora >= 18 && hora <= 23)
+            {
+                Console.WriteLine("Boa noite");
+            }
+            else
+            {
+                Console.WriteLine("Hora Invalida!");
+            }
+
+        }
+    }
+}
+
+/*
+             ///Simples
+            int x = 10;
+            
+            Console.WriteLine("Bom dia!");
+            if (x > 5)
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+            Console.WriteLine("Boa noite!");
+ */
+
+
+
+/*
+             //Composta
+            Console.WriteLine("Entre com um numero inteiro: ");
+            int x = int.Parse(Console.ReadLine());
+
+            if(x % 2 == 0)
+            {
+                Console.WriteLine("PAR!");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+ */
---
+++ Operadores_Comparativos_Logicos/Operadores_Comparativos_Logicos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Operadores_Comparativos_Logicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Operadores Comparativos
+            int a = 10;
+            bool c1 = a < 10;
+            Console.WriteLine(c1);
+
+            bool c2 = a < 20;
+            Console.WriteLine(c2);
+
+            bool c3 = a != 9;
+            Console.WriteLine(c3);
+
+            Console.WriteLine("=============================================");
+            //Operadores Lógicos
+            //&& -> and     || -> or        ! -> not
+            //precedencia: ! > && > ||
+            bool c4 = !(2 > 3) && 4 != 5;
+            Console.WriteLine(c4);
+
+            Console.WriteLine("=============================================");
+            bool c5 = 2 > 3 || 4 != 5; //true
+            bool c6 = !(2 > 3) && 4 != 5; //true
+            bool c7 = 10 < 5; //false
+            bool c8 = c5 || c6 && c7; //true
+
+            Console.WriteLine(c8);
+        }
+    }
+}
