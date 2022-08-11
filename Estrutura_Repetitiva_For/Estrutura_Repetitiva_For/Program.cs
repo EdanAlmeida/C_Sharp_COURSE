521b135f2da48473dcf746f8fcae89e4d5443f05
EdanAlmeida <edluisalmeida@gmail.com>
2022-08-11 15:20:25 -0300

    081122_3

---
+++ Estrutura_Repetitiva_For/Estrutura_Repetitiva_For/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Estrutura_Repetitiva_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sum 'N' values
+            Console.WriteLine("How many number would you like to insert? ");
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            
+            for (int i = 1; i <= n; i ++)
+            {
+                Console.Write("Type the value #" + i + " = ");
+                int value = int.Parse(Console.ReadLine());
+                sum += value;
+            }
+
+            Console.WriteLine("SUM = " + sum);
+
+        }
+    }
+}
+
+
+/*          
+           //Estrutura Repetitiva For
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("I = " + i);
+            }*/
+
---
+++ Exercicios_For/Exercicios_For/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Exercicios_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problema in/out intervalo
+            Console.Write("Informe um valor inteiro (1 < 'N' < 100): ");
+            int N = int.Parse(Console.ReadLine());
+            int somaIn = 0;
+            int somaOut = 0;
+            Console.WriteLine();
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.Write("Valor #" + i + " = ");
+                int valor = int.Parse(Console.ReadLine());
+                if (valor >= 10 && valor <= 20)
+                {
+                    somaIn += 1;
+                }
+                else
+                {
+                    somaOut += 1;
+                }
+            }
+
+            Console.WriteLine(somaIn + " In");
+            Console.WriteLine(somaOut + " Out");
+
+        }
+    }
+}
+
+
+/*
+            //Problemas ímpares até 'x'
+            Console.Write("Informe um valor inteiro (1 < 'X' < 100): ");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= x; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+ */
+
