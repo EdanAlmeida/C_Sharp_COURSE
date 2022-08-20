c0730b43683931dcea282f6f8de2785323dbbd1f
EdanAlmeida <edluisalmeida@gmail.com>
2022-08-20 11:45:34 -0300

    082022_1

---
+++ Exercicio_Matriz/Exercicio_Matriz/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Exercicio_Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of rows: ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("Enter the number of columns: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Enter the value of 'X': " );
+            int x = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[m, n]; //criação e instanciação da matriz
+
+            //for para armazenar os valores na matriz
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("Enter row #" + i + ", column #" + j + ": ");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+            //for para printar os valores na tela
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(mat[i, j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+
+            //for para verificar a exixtência do valor estipulado para x dentro da matriz
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] == x)
+                    {
+                        Console.WriteLine("Position: " + i + ", " + j);
+                        if (i > 0)
+                        {
+                            Console.WriteLine("UP: " + mat[i - 1, j]);
+                        }
+                        if (i < m - 1)
+                        {
+                            Console.WriteLine("DOWN " + mat[i + 1, j]);
+                        }
+                        if (j > 0)
+                        {
+                            Console.WriteLine("LEFT: " + mat[i, j - 1]);
+                        }
+                        if (j < n - 1)
+                        {
+                            Console.WriteLine("RIGHT: " + mat[i, j + 1]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Matrizes/Matrizes/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Activity Square array
+            Console.Write("Enter the array number: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            int soma = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                //string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("Enter the value of row #" + i + " Column #" + j + ": ");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(mat[i, j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Main Diagonal: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        soma ++;
+                    }
+                }
+            }
+            Console.WriteLine("Negative Numbers: " + soma);
+            
+
+        }
+    }
+}
+
+/*
+            double[,] mat = new double[2, 3];
+            Console.WriteLine(mat.Length);
+            Console.WriteLine(mat.Rank);
+            Console.WriteLine(mat.GetLength(0));//linhas
+            Console.WriteLine(mat.GetLength(1));//colunas
+ */
+
