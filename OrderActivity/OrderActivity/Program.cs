7ea3d3f329c554aea55f3595f6d21be47b65d37b
EdanAlmeida <edluisalmeida@gmail.com>
2022-09-09 08:50:27 -0300

    090922_1

--- OrderActivity/OrderActivity/Program.cs
+++ OrderActivity/OrderActivity/Program.cs
@@ -14,8 +14,39 @@ namespace OrderActivity
             string name = Console.ReadLine();
             Console.Write("Email: ");
             string email = Console.ReadLine();
-            Console.WriteLine("Birth date: (DD/MM/YYYY): ");
+            Console.Write("Birth date: (DD/MM/YYYY): ");
             DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.WriteLine();
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Enter #" + i + " item data: ");
+                Console.Write("Product name: ");
+                string pName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double pPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Product product = new Product(pName, pPrice);
+
+                Console.Write("Quantity: ");
+                int pQuant = int.Parse(Console.ReadLine());
+
+                OrderItem orderItem = new OrderItem(pQuant, pPrice, product);
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Order Summary: ");
+            Console.WriteLine(order);
         }
     }
 }
